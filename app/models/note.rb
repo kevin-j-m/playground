f3390b2ecc7751443c6cff2e44891f1606f7f810
Kevin Murphy <murphy.kevin.mail@gmail.com>
2022-10-01 07:54:07 -0400

    track evolution of note class for enumerating musical notes post

---
+++ app/models/note.rb
@@ -0,0 +1,50 @@
+class Note
+  def initialize(root_note:, root_octave:, offset:)
+    @note_cycle = notes.rotate(notes.index(root_note)).cycle
+    @root_octave = root_octave
+    @offset = offset
+  end
+
+  def note_progression
+    @note_cycle.take(@offset + 1)
+  end
+
+  def c_root?
+    @note_cycle.first == :c
+  end
+
+  def octaves_progressed
+    cs_passed = note_progression.count(:c)
+
+    if c_root?
+      cs_passed -= 1
+    end
+
+    cs_passed
+  end
+
+  def octave
+    @root_octave + octaves_progressed
+  end
+
+  def value
+    note_progression.last
+  end
+
+  def notes
+    [
+      :a_flat,
+      :a,
+      :b_flat,
+      :b,
+      :c,
+      :d_flat,
+      :d,
+      :e_flat,
+      :e,
+      :f,
+      :g_flat,
+      :g,
+    ]
+  end
+end
