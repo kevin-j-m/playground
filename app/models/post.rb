536b025b42303001386aeae4dc0541af766e49c5
Kevin Murphy <murphy.kevin.mail@gmail.com>
2023-04-17 16:06:45 -0400

    include code examples about mocking object in tests when a different object is returned from the implementation because of a DB call

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  def self.publish_all
+    Post.all.map(&:publish)
+  end
+
+  def publish; end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".publish_all" do
+    it "publishes all posts, but doesn't succeed because the mock is on a different object in memory" do
+      to_publish = Post.create
+      allow(to_publish).to receive(:publish)
+
+      Post.publish_all
+
+      expect(to_publish).to have_received(:publish)
+    end
+
+    it "publishes all post, testing the return behavior but using any instance of" do
+      # https://rspec.info/features/3-12/rspec-mocks/working-with-legacy-code/any-instance/
+      Post.create
+      allow_any_instance_of(Post).to receive(:publish).and_return("called publish")
+
+      expect(Post.publish_all).to eq ["called publish"]
+    end
+
+    it "publishes all post by forcing the DB call to return the object in memory" do
+      to_publish = Post.create
+      allow(Post).to receive(:all).and_return([to_publish])
+      allow(to_publish).to receive(:publish)
+
+      Post.publish_all
+
+      expect(to_publish).to have_received(:publish)
+    end
+  end
 end
