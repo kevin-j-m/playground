0208f967d4351cded6c738f777f94f3a137f4caa
Kevin Murphy <murphy.kevin.mail@gmail.com>
2025-03-05 21:03:56 -0500

    can't reproduce behavior under Rails 7.0

--- app/controllers/flash_quizzes_controller.rb
+++ app/controllers/flash_quizzes_controller.rb
@@ -1,12 +1,14 @@
 class FlashQuizzesController < ApplicationController
   def index
-    flash[:foo] = :bar
+    flash[:foo] = "bar"
   end
 
   def show
-    if params[:my_param] == "t"
+    if params[:redirect_to_edit] == "and_log"
       _value_to_use = flash[:baz]
-      redirect_to edit_flash_quiz_path
+      redirect_to edit_flash_quiz_path(params[:id])
+    elsif params[:redirect_to_edit].present?
+      redirect_to edit_flash_quiz_path(params[:id])
     end
   end
 
--- spec/requests/flash_quizzes_spec.rb
+++ spec/requests/flash_quizzes_spec.rb
@@ -6,12 +6,39 @@ RSpec.describe "flash quizzes", type: :request do
       get flash_quizzes_path
 
       expect(response).to be_successful
-      expect(flash[:foo]).to eq :bar
+      expect(flash[:foo]).to eq "bar"
     end
   end
 
   describe "GET /show" do
+    it "is successful" do
+      get flash_quiz_path(1)
 
+      expect(response).to be_successful
+    end
+
+    it "redirects to the edit page when flashing the secret parameter" do
+      get flash_quiz_path(1, redirect_to_edit: "t")
+
+      expect(response).to redirect_to edit_flash_quiz_path(1)
+    end
+
+    # TODO: does this fail under  rails (7.1.5.1)?
+    it "retains the flash value when redirecting and logging" do
+      get flash_quizzes_path
+      expect(flash[:foo]).to eq "bar"
+
+      get flash_quiz_path(1, redirect_to_edit: "and_log")
+      expect(flash[:foo]).to eq "bar"
+    end
+
+    it "retains the flash value when redirecting and not logging" do
+      get flash_quizzes_path
+      expect(flash[:foo]).to eq "bar"
+
+      get flash_quiz_path(1, redirect_to_edit: "r")
+      expect(flash[:foo]).to eq "bar"
+    end
   end
 
   describe "GET /edit" do
